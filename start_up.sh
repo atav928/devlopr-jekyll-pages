5aab7e19d6c1b0f0fd5b1eaaa4888f5500a708db
atav928 <31765990+atav928@users.noreply.github.com>
2024-05-12 21:08:46 -0400

    updated script

--- start_up.sh
+++ start_up.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-prod.yml up  
+#docker-compose -f docker-compose-prod.yml up  
+docker build -t my-devlopr-jekyll-blog .
+docker run -d -p 4000:4000 -it --volume="$PWD:/srv/jekyll" --name "my_blog" my-devlopr-jekyll-blog:latest jekyll serve
